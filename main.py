8332ef7f5c45caa16f90896a4634048809583cc1
utisz86 <soma.kormanik@outlook.com>
2020-07-11 02:10:34 +0300

    Data in out, spinbox

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from tkinter import Tk, Label, Button, Frame, RAISED, Spinbox
+from tkinter import Tk, Label, Button, Frame, RAISED, Spinbox, DoubleVar
 from WeekClass import WeekClass
 
 import datetime
 import pickle
+
+from chunks import chunks
 
 class MyFirstGUI:
     def __init__(self, master, weekdata):
@@ -14,6 +16,8 @@ class MyFirstGUI:
         borderwidth=1
         )
 
+        self.spinboxs = []
+
         # Week days
         for (i, day) in enumerate(["{1}: {0} week".format(weekdata.weekNumber, weekdata.weekyear)]+weekdata.weekDays+["Min", "Max"]):
             self.label = Label(master=master, text=day)
@@ -24,13 +28,6 @@ class MyFirstGUI:
             self.label = Label(master=master, text=event)
             self.label.grid(row=j+1, column=0)
 
-        # Spinboxes
-        for (j, event) in enumerate(weekdata.foodEvents):
-            for (i, day) in enumerate(weekdata.weekDays):
-                # Spinbox default value
-                self.spinbox = Spinbox(master, from_=weekdata.data[j][i], to=10, width=5)
-                self.spinbox.grid(row=j+1, column=i+1)
-                
         # Min-Max value
         for (j, event) in enumerate(weekdata.foodEvents):
             self.label = Label(master=master, text=0)
@@ -38,6 +35,34 @@ class MyFirstGUI:
 
             self.label = Label(master=master, text=0)
             self.label.grid(row=j+1, column=len(weekdata.weekDays)+2)
+
+        # Spinboxes
+        for (j, event) in enumerate(weekdata.foodEvents):
+            for (i, day) in enumerate(weekdata.weekDays):
+                # Spinbox default value
+                var = DoubleVar(value=weekdata.data[j][i])  # initial value
+                self.spinbox = Spinbox(master, from_=0, to=10, width=5, command = self.spinCallBack, textvariable=var)
+                self.spinbox.grid(row=j+1, column=i+1)
+                self.spinboxs.append(self.spinbox)
+                
+
+    # Comand function when spinbox change
+    # If spinbox change, read the new data and input into weekdata
+    def spinCallBack(self):
+        # Refress all of the data
+        spin_index = chunks(range(len(self.spinboxs)), len(weekdata.weekDays))
+        for (j, event) in enumerate(weekdata.foodEvents):
+            for (i, day) in enumerate(weekdata.weekDays):
+                weekdata.data[j][i] = self.spinboxs[spin_index[j][i]].get()
+
+        with open("week"+str(year)+str(week_num)+"_data.pkl", 'wb') as output:
+            pickle.dump(weekdata, output, pickle.HIGHEST_PROTOCOL)
+
+        
+           
+
+
+        
 
 if __name__ == "__main__":
     # This week data
---
+++ tests/test_chunks.py
@@ -0,0 +1,24 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'Smartplate')))
+
+from chunks import chunks
+
+def test(successful_test):
+    """ The print of test results.    """
+    sorszam = sys._getframe(1).f_lineno
+    if successful_test:
+        msg = "The row of {0}. passed successfully.".format(sorszam)
+    else:
+        msg = ("The row of {0}. FAILED.".format(sorszam))
+
+    print(msg)
+
+def test_sets():
+    """ Run the test set for this module. """
+    test(abs(-5) == 5)
+    #
+    test(chunks([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 4) == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+
+
+
+test_sets()
