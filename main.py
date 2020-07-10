a8f783e0866bf505afdbc5bc3734489631574446
utisz86 <soma.kormanik@outlook.com>
2020-07-10 22:31:10 +0300

    Appearance 0.0

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-import tkinter as tk
+from tkinter import Tk, Label, Button, Frame, RAISED, Spinbox
 
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Smartplate")
 
-# Main application 
-class MainApplication(tk.Frame):
-    """[This class holds together the whole application]
+        self.frame = Frame(master=master,
+        relief = RAISED,
+        borderwidth=1
+        )
 
-    Args:
-        tk ([type]): [Tkinter Frame class]
-    """
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
+        # Week days
+        for (i, day) in enumerate(["{1}: {0} week".format(1, 2020)]+["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]+["Min", "Max"]):
+            self.label = Label(master=master, text=day)
+            self.label.grid(row=0, column=i)
 
+        # FoodEvents
+        for (j, event) in enumerate(["Zöldség", "Gyümölcs", "Gabona", "Hús", "Friss hal","Belsöség", "Édesség", "Magvak", "Üditő", "Sport", "Súly"]):
+            self.label = Label(master=master, text=event)
+            self.label.grid(row=j+1, column=0)
+
+        # Spinboxes
+        for (j, event) in enumerate(["Zöldség", "Gyümölcs", "Gabona", "Hús", "Friss hal","Belsöség", "Édesség", "Magvak", "Üditő", "Sport", "Súly"]):
+            for (i, day) in enumerate(["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]):
+                self.spinbox = Spinbox(master, from_=0, to=10, width=5)
+                self.spinbox.grid(row=j+1, column=i+1)
+                
+        # Min-Max value
+        for (j, event) in enumerate(["Zöldség", "Gyümölcs", "Gabona", "Hús", "Friss hal","Belsöség", "Édesség", "Magvak", "Üditő", "Sport", "Súly"]):
+            self.label = Label(master=master, text=0)
+            self.label.grid(row=j+1, column=8)
+
+            self.label = Label(master=master, text=0)
+            self.label.grid(row=j+1, column=9)
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root = Tk()
+    my_gui = MyFirstGUI(root)
     root.mainloop()
         
