0ac50aa7b84ddd42fc7a399b6cb0ac8a03cc571d
utisz86 <soma.kormanik@outlook.com>
2020-07-10 22:39:53 +0300

    WeekClass structure 0.0

--- WeekClass.py
+++ WeekClass.py
@@ -1,30 +1,14 @@
-import sys
+import datetime
 
-def test(successful_test):
-    """ The print of test results.    """
-    sorszam = sys._getframe(1).f_lineno
-    if successful_test:
-        msg = "The row of {0}. passed successfully.".format(sorszam)
-    else:
-        msg = ("The row of {0}. FAILED.".format(sorszam))
-
-    print(msg)
-
-def test_sets():
-    """ Run the test set for this module. """
-    test(abs(-5) == 5)
-    #
-    testWeek = WeekClass(foodEvents=["Zöldség", "Gyümölcs", "Gabona"], weekDays =["Monday","Tuesday","Wednesday","Thursday"] )
-    test(testWeek.data == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
-    testWeek = WeekClass()
+my_date = datetime.date.today()
+year, week_num, day_of_week = my_date.isocalendar()
 
 class WeekClass:
-    
-
     def __init__(self, foodEvents=["Zöldség", "Gyümölcs", "Gabona", "Hús", "Friss hal","Belsöség", "Édesség", "Magvak", "Üditő", "Sport", "Súly"], weekDays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]):
+        self.weekyear = year
+        self.weekNumber = week_num
         self.foodEvents = foodEvents
         self.weekDays = weekDays
         self.data = [[0] * len(weekDays) for i in range(len(foodEvents))]
         
-
-test_sets()        
+     
